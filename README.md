1d7fad6b8f1f24a304449e0e717f59e348705013
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2021-12-22 11:18:01 +0100

    [TASK] Base files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Ad server for TYPO3
+
+This extension provides a versatile for [TYPO3](https://typo3.org/).
+
+---
+
+**Note: This project is currently under development!**
+
+---
+
+## Features
+
+* a
+* b
+* c
+
+## Installation
+
+Composer package
+
+```
+composer require aaw-team/adserver
+```
+
+## Docs
+
+## License
+
+GPLv2.0 or later
+
+## Copyright
+
+2021 by Agentur am Wasser | Maeder & Partner AG (https://www.agenturamwasser.ch)
---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Copyright by Agentur am Wasser | Maeder & Partner AG
+ *
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Ad server for TYPO3',
+    'description' => '',
+    'category' => 'fe',
+    'author' => 'Agentur am Wasser | Maeder & Partner AG',
+    'author_email' => 'development@agenturamwasser.ch',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => true,
+    'version' => '1.0.0-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.22-10.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
